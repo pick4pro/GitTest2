483bb5076e3a1336e412338e81f4937fc466ee52
Jimmy J Wright <jimjrhino@gmail.com>
2018-11-18 18:05:15 -0700

    Initial commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by Jimmy Wright on 11/18/18.
+//  Copyright © 2018 Jimmy Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
